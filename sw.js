216c623546ac2615b6427ec291ec25cbed8aa084
miihzteir <miihzteir@gmail.com>
2026-09-02 16:35:14 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="meu-estudo-completo-v4",FILES=["./","index.html","style.css","app.js","icone.svg","manifest.webmanifest"];
+self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)))});
+self.addEventListener("activate",event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))])));
+self.addEventListener("fetch",event=>event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))));
